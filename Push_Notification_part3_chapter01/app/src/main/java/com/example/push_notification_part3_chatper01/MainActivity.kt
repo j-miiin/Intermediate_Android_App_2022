bc011b50b586fb0cf2a58b3bc917c7a3f17aa35c
jungmin <kimjm0825@gmail.com>
2022-07-13 01:56:18 +0900

    푸시 알림 수신기 완성

--- Push_Notification_part3_chapter01/app/src/main/java/com/example/push_notification_part3_chatper01/MainActivity.kt
+++ Push_Notification_part3_chapter01/app/src/main/java/com/example/push_notification_part3_chatper01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.push_notification_part3_chatper01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -20,6 +21,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -29,5 +38,14 @@ class MainActivity : AppCompatActivity() {
                     firebaseToken.text = task.result
                 }
             }
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다"
+                } else {
+                    "(으)로 실행했습니다"
+               }
     }
 }
