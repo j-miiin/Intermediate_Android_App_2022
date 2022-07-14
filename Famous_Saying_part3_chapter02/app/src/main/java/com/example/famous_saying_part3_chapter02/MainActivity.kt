c6cf424e2de9ab86b153d134b631ef42e999edfb
jungmin <kimjm0825@gmail.com>
2022-07-14 18:41:01 +0900

    Remote Config 연동 및 레이아웃 수정

--- Famous_Saying_part3_chapter02/app/src/main/java/com/example/famous_saying_part3_chapter02/MainActivity.kt
+++ Famous_Saying_part3_chapter02/app/src/main/java/com/example/famous_saying_part3_chapter02/MainActivity.kt
@@ -2,7 +2,14 @@ package com.example.famous_saying_part3_chapter02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,14 +17,57 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.viewPager)
     }
 
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initViews()
+        initData()
     }
 
-    private fun initViews() {
-        viewPager.adapter = QuotesPagerAdapter(emptyList())
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0   // 앱을 들어올 때마다 fetch가 됨
+            }
+        )
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+
+            if (it.isSuccessful) {
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quotes, isNameRevealed)
+            }
+        }
+    }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let {
+                jsonList = jsonList + it
+            }
+        }
+
+        return jsonList.map {
+            Quote(
+                quote = it.getString("quote"),
+                name = it.getString("name"))
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        viewPager.adapter = QuotesPagerAdapter(
+            quotes = quotes,
+            isNameRevealed = isNameRevealed
+        )
     }
 }
