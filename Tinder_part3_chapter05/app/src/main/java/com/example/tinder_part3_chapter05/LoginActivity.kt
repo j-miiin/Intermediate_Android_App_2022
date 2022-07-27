6882a0712eaf440d1670f479dc5f1c34af384a8f
jungmin <kimjm0825@gmail.com>
2022-07-27 23:11:30 +0900

    Facebook 로그인 구현 및 Firebase RealtimeDatabase 연동

---
+++ Tinder_part3_chapter05/app/src/main/java/com/example/tinder_part3_chapter05/LoginActivity.kt
@@ -0,0 +1,152 @@
+package com.example.tinder_part3_chapter05
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth    // FirebaseAuth.getInstance() 와 같은 코드인데 좀 더 코틀린스러운 코드
+        callbackManager = CallbackManager.Factory.create()
+
+        initLoginButton()
+        initSignupButton()
+        initEmailAndPasswordEditText()
+        initFacebookLoginButton()
+    }
+
+    private fun initLoginButton() {
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        handleSuccessLogin()    // 로그인을 성공하면 Activity 종료
+                    } else {
+                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun initSignupButton() {
+        val signupButton = findViewById<Button>(R.id.signupButton)
+        signupButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun initEmailAndPasswordEditText() {
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signupButton = findViewById<Button>(R.id.signupButton)
+
+        // 이메일, 패스워드 창이 비어있으면 로그인, 회원가입 버튼을 비활성화
+        emailEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signupButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signupButton.isEnabled = enable
+        }
+    }
+
+    private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
+
+        facebookLoginButton.setPermissions("email", "public_profile")   // email과 public profile 정보를 가져오겠다
+        facebookLoginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {
+                // 로그인 성공
+                // 로그인 access token을 firebase에 넘겨줘야 함
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) { task ->
+                        if (task.isSuccessful) {
+                            handleSuccessLogin()
+                        } else {
+                            Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+            override fun onCancel() {
+                // 로그인을 하다 취소했을 경우
+            }
+
+            override fun onError(error: FacebookException?) {
+                // 로그인 실패
+                Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun getInputEmail(): String {
+        return findViewById<EditText>(R.id.emailEditText).text.toString()
+    }
+
+    private fun getInputPassword(): String {
+        return findViewById<EditText>(R.id.passwordEditText).text.toString()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleSuccessLogin() {
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val userId = auth.currentUser?.uid.orEmpty()
+        val currentUserDB = Firebase.database.reference.child("Users").child(userId)
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        currentUserDB.updateChildren(user)
+
+        finish()
+    }
+}
