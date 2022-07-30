b0e1c1a0e8064f64dace0f466cdfeb3640c2dec7
jungmin <kimjm0825@gmail.com>
2022-07-31 01:35:01 +0900

    Home Fragment 코드 작성

---
+++ Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/home/ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.example.carrot_market_part3_chapter06.home
+
+data class ArticleModel(
+    val sellerId: String,
+    val title: String,
+    val createdAt: Long,
+    val price: String,
+    val imageUrl: String
+)
--- Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/home/HomeFragment.kt
+++ Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/home/HomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.carrot_market_part3_chapter06.R
+import com.example.carrot_market_part3_chapter06.databinding.FragmentHomeBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -13,6 +15,25 @@ private const val ARG_PARAM2 = "param2"
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private var param1: String? = null
     private var param2: String? = null
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0",  "aaaa", 1000000, "5000원", ""))
+            add(ArticleModel("0",  "aaaa", 2000000, "5000원", ""))
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +47,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
