4e296c0786ca7ae26e5325195969da4d7953f1a4
jungmin <kimjm0825@gmail.com>
2022-08-02 18:34:27 +0900

    Firebase에서 상품 목록 가져오기 구현

---
+++ Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/DBKey.kt
@@ -0,0 +1,7 @@
+package com.example.carrot_market_part3_chapter06
+
+class DBKey {
+    companion object {
+        const val DB_ARTICLES = "Articles"
+    }
+}
--- Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/home/HomeFragment.kt
+++ Carrot_Market_part3_chapter06/app/src/main/java/com/example/carrot_market_part3_chapter06/home/HomeFragment.kt
@@ -6,18 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.carrot_market_part3_chapter06.DBKey.Companion.DB_ARTICLES
 import com.example.carrot_market_part3_chapter06.R
 import com.example.carrot_market_part3_chapter06.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var articelDB: DatabaseReference
+    private lateinit var articleAdapter: ArticleAdapter
+
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+    }
 
     private var binding: FragmentHomeBinding? = null
-    private lateinit var articleAdapter: ArticleAdapter
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,39 +56,26 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val fragmentHomeBinding = FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
 
+        articleList.clear()
+        articelDB = Firebase.database.reference.child(DB_ARTICLES)
         articleAdapter = ArticleAdapter()
-        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
-            add(ArticleModel("0",  "aaaa", 1000000, "5000원", ""))
-            add(ArticleModel("0",  "aaaa", 2000000, "5000원", ""))
-        })
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        articelDB.addChildEventListener(listener)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onResume() {
+        super.onResume()
+
+        articleAdapter.notifyDataSetChanged()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        articelDB.removeEventListener(listener)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
